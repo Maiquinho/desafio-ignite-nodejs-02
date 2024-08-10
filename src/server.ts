52981edf50ffb596dd9d49b7d937692b447adc39
Michael Matheus <michaelmatheus2001@gmail.com>
2024-08-10 18:22:27 -0300

    feat: add environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -11,8 +12,6 @@ app.get('/', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
-  .then(() => {
-    console.log('HTTP Server running...')
-  })
+  .then(() => console.log('HTTP Server running...'))
