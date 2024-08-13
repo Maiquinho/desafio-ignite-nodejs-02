0c10ab678a733cfa88505a2ba1e3f22df9b321a6
Michael Matheus <michaelmatheus2001@gmail.com>
2024-08-13 17:14:19 -0300

    fix(migrations): set email column as unique

--- db/migrations/20240812165425_create-users.ts
+++ db/migrations/20240812165425_create-users.ts
@@ -4,8 +4,8 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('users', (table) => {
     table.uuid('id').primary()
     table.uuid('session_id').index()
-    table.string('name').notNullable().unique()
-    table.string('email').notNullable()
+    table.string('name').notNullable()
+    table.string('email').notNullable().unique()
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
     table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
   })
